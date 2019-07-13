0308db1892d04c9f914d216fc0a5dbff6bdfb3c8
Denis Kalyan <denis_kalyan@hotmail.com>
2019-07-13 16:53:14 +0100

    Added Pclass Image

--- Train_Data_Analysis.py
+++ Train_Data_Analysis.py
@@ -20,7 +20,7 @@ sur = "Survived"
 not_sur = "Not Survived"
 new_df = df.head(5)
 #Creating a plot to see the different sex and age of the survivors compared to non survivors
-fig, axes = plt.subplots(nrows=1, ncols=2,figsize=(10,4))
+#fig, axes = plt.subplots(nrows=1, ncols=2,figsize=(10,4))
 men = df[df["Sex"]== "male"]
 female = df[df["Sex"] == "female"]
 women_sur = female[female["Survived"] == 1]
@@ -28,7 +28,7 @@ women_not_sur = female[female["Survived"] == 0]
 men_sur = men[men["Survived"] == 1]
 men_not_sur = men[men["Survived"] == 0]
 
-'''MALE'''
+"""'''MALE'''
 ax = sns.distplot(men_sur.Age.dropna(), bins=18, label = sur, color= "g", ax = axes[1], kde =False)
 ax = sns.distplot(men_not_sur.Age.dropna(), bins=40, label = not_sur, color="r",  ax = axes[1], kde =False)
 ax.legend()
@@ -38,13 +38,13 @@ ax.set_title("Male")
 ax = sns.distplot(women_sur.Age.dropna(),bins=18, label = sur, color= "g", ax = axes[0], kde =False)
 ax = sns.distplot(women_not_sur.Age.dropna(), bins=40, label = not_sur, color="r",  ax = axes[0], kde =False)
 ax.legend()
-ax.set_title("Female")
+ax.set_title("Female")"""
 
 
 #Comparing different ticket classes to survival by where they embarked their journey 
-'''FacetGrid = sns.FacetGrid(df, row='Embarked', size=4.5, aspect=1.6)
+FacetGrid = sns.FacetGrid(df, row='Embarked', size=3, aspect=1.6)
 FacetGrid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette=None,  order=None, hue_order=None )
-FacetGrid.add_legend()'''
+FacetGrid.add_legend()
 
 #PClass in comparison to Survival
 #sns.barplot(x='Pclass', y='Survived', data=df)
