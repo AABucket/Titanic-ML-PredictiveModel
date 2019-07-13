278f3199a36c4f7ed13b16a5016457f5f3072ea4
Denis Kalyan <denis_kalyan@hotmail.com>
2019-07-13 16:26:30 +0100

    gender/age image

--- Train_Data_Analysis.py
+++ Train_Data_Analysis.py
@@ -20,7 +20,7 @@ sur = "Survived"
 not_sur = "Not Survived"
 new_df = df.head(5)
 #Creating a plot to see the different sex and age of the survivors compared to non survivors
-#fig, axes = plt.subplots(nrows=1, ncols=2,figsize=(10,4))
+fig, axes = plt.subplots(nrows=1, ncols=2,figsize=(10,4))
 men = df[df["Sex"]== "male"]
 female = df[df["Sex"] == "female"]
 women_sur = female[female["Survived"] == 1]
@@ -28,18 +28,17 @@ women_not_sur = female[female["Survived"] == 0]
 men_sur = men[men["Survived"] == 1]
 men_not_sur = men[men["Survived"] == 0]
 
-"""#Drawing Plots
+'''MALE'''
+ax = sns.distplot(men_sur.Age.dropna(), bins=18, label = sur, color= "g", ax = axes[1], kde =False)
+ax = sns.distplot(men_not_sur.Age.dropna(), bins=40, label = not_sur, color="r",  ax = axes[1], kde =False)
+ax.legend()
+ax.set_title("Male")
+
 '''FEMALE'''
 ax = sns.distplot(women_sur.Age.dropna(),bins=18, label = sur, color= "g", ax = axes[0], kde =False)
 ax = sns.distplot(women_not_sur.Age.dropna(), bins=40, label = not_sur, color="r",  ax = axes[0], kde =False)
 ax.legend()
 ax.set_title("Female")
-
-'''MALE'''
-ax = sns.distplot(men_sur.Age.dropna(), bins=18, label = sur, color= "g", ax = axes[1], kde =False)
-ax = sns.distplot(men_not_sur.Age.dropna(), bins=40, label = not_sur, color="r",  ax = axes[1], kde =False)
-ax.legend()
-ax.set_title("Male")"""
 
 
 #Comparing different ticket classes to survival by where they embarked their journey 
